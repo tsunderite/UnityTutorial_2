5c5c087cc8da86318f7ff088f141532c03a96cb3
Mika <mikkivega2001@gmail.com>
2023-03-07 12:00:53 -0500

    Main Files

---
+++ UnityTutorial_2/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
+}
